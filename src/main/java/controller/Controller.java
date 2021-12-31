0acfab866dab5498533aefc78db0b13449364566
Laure Runser <laure-runser@tutanota.com>
2021-12-31 12:46:49 +0100

    end of doc

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -11,25 +11,59 @@ import java.io.File;
 import java.io.IOException;
 import java.util.stream.IntStream;
 
-// TODO doc
-
 public class Controller {
+    /**
+     * The FractalGenerator.
+     * Can be null at the beginning if in GUI mode,
+     * and no Fractal has been generated yet.
+     */
     private FractalGenerator generator;
+
+    /**
+     * The GUI view.
+     * Can be null if in CLI mode.
+     */
     private GUI view;
+
+    /**
+     * The generated fractal.
+     */
     private BufferedImage image;
 
+    /**
+     * Creates a new Controller for the CLI mode.
+     *
+     * @param generator the Fractal Generator
+     */
     public Controller(FractalGenerator generator) {
         this.generator = generator;
     }
+
+    /**
+     * Creates a new Controller for the GUI mode.
+     * It immediatly creates a GUI view and makes it visible.
+     */
     public Controller() {
         view = new GUI(this);
     }
 
+    /**
+     * Set a new FractalGenerator, then computes and draws the fractal
+     *
+     * @param generator the new FractalGenerator
+     */
     public void setGenerator(FractalGenerator generator) {
         this.generator = generator;
         view.getImagePanel().addImageDisplay(generator.getWidth(), generator.getHeight());
     }
 
+    /**
+     * Saves the generated fractal image
+     *
+     * @param makeImage if true, computes the image beforehand
+     * @param filename  the name of the file to save to
+     * @param colorCode the color code to use (if generating the image
+     */
     public void saveImage(boolean makeImage, String filename, ColorCode.Code colorCode) {
         // TODO also save a .txt with the info about the fractal
         // use the toString that was already coded in the Generators
@@ -38,7 +72,7 @@ public class Controller {
         }
 
         File outputfile = new File(filename);
-        if(makeImage) {
+        if (makeImage) {
             generator.computeDivergenceIndex();
             image = makeImage(colorCode);
         }
@@ -50,6 +84,11 @@ public class Controller {
         }
     }
 
+    /**
+     * Compute the fractal for the given range
+     *
+     * @param range the range to consider. If it is null, use the entire range.
+     */
     public void computeFractal(Rectangle2D.Double range) {
         if (range != null) {
             generator.computeDivergenceIndex(range);
@@ -58,6 +97,12 @@ public class Controller {
         }
     }
 
+    /**
+     * Make the image from the already computed divergence index.
+     *
+     * @param colorCode the color code to use
+     * @return the BufferedImage of the fractal
+     */
     public BufferedImage makeImage(ColorCode.Code colorCode) {
         int width = generator.getWidth();
         int height = generator.getHeight();
@@ -68,13 +113,20 @@ public class Controller {
         IntStream.range(0, width).parallel().forEach(x ->
                 IntStream.range(0, height).parallel().forEach(y ->
                         image.setRGB(x, y, ColorCode.getColor(divergenceIndex[x][y], generator.getMaxIter(), colorCode))));
-
+        // this parallel Stream doesn't seam to make much of a difference, at least for smaller images.
+        // But it doesn't hurt, and it might help for huge sizes.
         return image;
     }
 
+    /**
+     * Moves inside of the fractal and redraws the image
+     *
+     * @param horizontal true = left, false = right
+     * @param direction  true = up, false = down
+     */
     public void move(boolean horizontal, boolean direction) {
         Rectangle2D.Double range = generator.getRange();
-        int dir = direction?-1:1;
+        int dir = direction ? -1 : 1;
         if (horizontal) {
             range.x = range.x + dir * (range.width / 3);
         } else {
@@ -84,11 +136,21 @@ public class Controller {
         // TODO : move without recomputing the entire image
     }
 
+    /**
+     * Resets the fractal to the original range and redraws the image
+     */
     public void resetFractal() {
         generator.resetRange();
         view.getImagePanel().drawImage(true);
     }
 
+    /**
+     * Zooms in (or out) of the fractal and redraws the image
+     *
+     * @param x           the x-coordinate of the new center
+     * @param y           the y-coordinate of the new center
+     * @param scaleFactor the zoom factor
+     */
     public void zoom(int x, int y, double scaleFactor) {
         Rectangle2D.Double range = generator.getRange();
         // scaleFactor*2 because we only need half of the
@@ -102,6 +164,11 @@ public class Controller {
         view.getImagePanel().drawImage(true);
     }
 
+    /**
+     * Zooms in (or out) on the center point of the picture
+     *
+     * @param scaleFactor the zoom factor
+     */
     public void zoom(double scaleFactor) {
         zoom(generator.getWidth() / 2, generator.getHeight() / 2, scaleFactor);
     }
--- src/main/java/model/ColorCode.java
+++ src/main/java/model/ColorCode.java
@@ -2,13 +2,15 @@ package model;
 
 import java.awt.*;
 
+/**
+ * This class modelises the various color codes
+ */
 public class ColorCode {
-    public enum Code {
-        HUE1,
-        HUE2,
-        HUE3,
-    }
 
+    /**
+     * @param str a string with the name of the color code
+     * @return the Code that corresponds
+     */
     public static ColorCode.Code fromString(String str) {
         return switch (str) {
             case "HUE1" -> Code.HUE1;
@@ -18,6 +20,12 @@ public class ColorCode {
         };
     }
 
+    /**
+     * @param indice   the divergence index
+     * @param max_iter the maximum number of iterations
+     * @param code     the color code
+     * @return the rgb color
+     */
     public static int getColor(double indice, int max_iter, Code code) {
         if (indice == max_iter - 1) return Color.black.getRGB();
         return switch (code) {
@@ -27,16 +35,38 @@ public class ColorCode {
         };
     }
 
+    /**
+     * @param indice   the divergence index
+     * @param max_iter the maximum number of iterations
+     * @return the rgb color
+     */
     private static int getHue(double indice, int max_iter) {
-        return Color.HSBtoRGB((float)indice/max_iter, 0.7f, 0.7f);
+        return Color.HSBtoRGB((float) indice / max_iter, 0.7f, 0.7f);
     }
 
+    /**
+     * @param indice the divergence index
+     * @return the rgb color
+     */
     private static int getHue2(double indice) {
         return Color.HSBtoRGB((float) indice / 100.0F, 1F, 1F);
     }
 
+    /**
+     * @param indice the divergence index
+     * @return the rgb color
+     */
     private static int getHue3(double indice) {
         float hue = 0.7f + (float) indice / 200f;
         return Color.HSBtoRGB(hue, 1f, 1f);
+    }
+
+    /**
+     * The enum of all available color codes
+     */
+    public enum Code {
+        HUE1,
+        HUE2,
+        HUE3,
     }
 }
